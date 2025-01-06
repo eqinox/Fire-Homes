b1fce4bd26527759b6e10d928ca9f42bc2e59a77
VASKO\eqinox <eqinox@abv.bg>
2025-01-06 18:22:12 +0200

    add new property form

---
+++ src/app/admin-dashboard/new/new-property-form.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import PropertyForm from "@/components/property-form";
+import { propertyDataSchema } from "@/validation/propertySchema";
+import { PlusCircleIcon } from "lucide-react";
+import { z } from "zod";
+
+export default function NewPropertyForm() {
+  const handleSubmit = async (data: z.infer<typeof propertyDataSchema>) => {
+    console.log("data", data);
+  };
+
+  return (
+    <div>
+      <PropertyForm
+        handleSubmit={handleSubmit}
+        submitButtonLabel={
+          <>
+            <PlusCircleIcon /> Create Property
+          </>
+        }
+      />
+    </div>
+  );
+}
---
+++ src/validation/propertySchema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const propertyDataSchema = z.object({
+  address1: z.string().min(1, "Address line must contain a value"),
+  address2: z.string().optional(),
+  city: z.string().min(3, "City must contain at least 3 characters"),
+  postcode: z.string().refine((postcode) => {
+    const postcodeRegex = /\d{4}/i;
+    return postcodeRegex.test(postcode);
+  }, "Invalid UK postcode"),
+  price: z.coerce.number().positive("Price must be greater than zero"),
+  description: z
+    .string()
+    .min(40, "Description must contain at least 40 characters"),
+  bedrooms: z.coerce.number().min(0, "Bedrooms must be at least 0"),
+  bathrooms: z.coerce.number().min(0, "Bathrooms must be at least 0"),
+  status: z.enum(["draft", "for-sale", "withdrawn", "sold"]),
+});
