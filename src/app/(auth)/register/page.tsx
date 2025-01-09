65eb8f4eaf0dd772270d6e4f491265994773568a
VASKO\eqinox <eqinox@abv.bg>
2025-01-09 16:20:27 +0200

    register user with email and password

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,15 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import RegisterForm from "./register-form";
+
 export default function Register() {
-    return <div>
-        Register page
-    </div>
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-3xl font-bold">Register</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <RegisterForm />
+      </CardContent>
+    </Card>
+  );
 }
